dd2d7d56a06994387d84f46a93c99c44baf29e47
Adem AKKUŞ <32845253+ademakkus@users.noreply.github.com>
2021-07-09 18:23:52 +0300

    Add files via upload

---
+++ CapitalLetterToUpperWindowsFormsApp/Form1.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace CapitalLetterToUpperWindowsFormsApp
+{
+  public partial class Form1 : Form
+  {
+    public Form1()
+    {
+      InitializeComponent();
+    }
+
+    private void textBox1_TextChanged(object sender, EventArgs e)
+    {
+      if (textBox1.Text.Length == 1)
+      {
+        string ilk = textBox1.Text.Substring(0, 1);
+        ilk = ilk.ToUpper();
+        textBox1.Text = ilk;
+        textBox1.Select(textBox1.Text.Length, 1);   //imleci string sonuna konumlandırma
+      }
+    }
+  }
+}
